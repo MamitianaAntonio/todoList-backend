add2f5f29e53a5123045f03ff06db3976b641202
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-08-13 22:14:52 +0000

    feat : add json files utils to simplify the controller code

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,2 +1,13 @@
-import fs from "fs";
-import path from "path";
+import { readTodos, writeTodos } from "../utils/fileUtils.js";
+
+// get all todos in the json file
+const getTodos = (_req, res) => {
+  const todos = readTodos();
+  res.json(todos);
+};
+
+const addTodo = () => {};
+
+const toogleTodo = () => {};
+
+const deleteTodo = () => {};
